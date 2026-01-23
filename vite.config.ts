1ef5cb1549ccc63157e4900f9f15073d41465e46
Lorydera <chideraglory5@gmail.com>
2026-01-23 08:55:53 +0100

    fixed image file path

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: "/eke-glory-dev/",
-   
+
   server: {
     host: "::",
     port: 8080,
